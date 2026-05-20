8c5c3755ea937d73264e83563e7709e43c490abb
Ridho Idris <ridhoidris@gmail.com>
2026-05-20 16:10:25 +0700

    mobile: centralize beranda icon asset mapping

---
+++ apps/mobile/src/lib/app-icons.ts
@@ -0,0 +1,8 @@
+export const appIcons = {
+  scan: require('../../assets/icons/scan.png'),
+  dompet: require('../../assets/icons/dompet.png'),
+  transaksi: require('../../assets/icons/transaksi.png'),
+  aset: require('../../assets/icons/aset.png'),
+  tagihan: require('../../assets/icons/tagihan.png'),
+  goal: require('../../assets/icons/goal.png'),
+} as const;
